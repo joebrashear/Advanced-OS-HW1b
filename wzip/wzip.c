7556f9af2079a9d1ac4e858a357a0d1c1d15647a
joebrashear <joebrashear@live.com>
2023-01-29 10:59:19 -0500

    Rewrote compress algorithm, works

--- wzip/wzip.c
+++ wzip/wzip.c
@@ -6,43 +6,32 @@
 
 int debug = 0;
 
-void compress(FILE *fp)
+int compress(FILE *fp)
 {
-    char *text = NULL;
-    size_t buffer_len = 0;
-    int left = 0;
-    int right = 0;
-    int len = 0;
+    char left = fgetc(fp);
+    char right;
     int substring_len = 0;
+    int ret = 0;
 
-    while (getline(&text, &buffer_len, fp) != -1) 
+    while (left != EOF) 
     {
-        left = 0;
-        substring_len = 0;
-        len = strlen(text);
+        substring_len = 1;
+        
+        while ((right = fgetc(fp)) != EOF && left == right)
+            substring_len++;
 
-        for (right = 1; right < len; right++)
+        ret = fwrite(&substring_len, sizeof(substring_len), 1, stdout);
+        if (ret != 1)
         {
-            substring_len = right - left;
+            fprintf(stderr, "fwrite failed\n");
+            return 1;
+        }
 
-            if (text[left] != text[right])
-            {
-                if (debug == 1)
-                {
-                    printf("%d%c", substring_len, text[left]);
-                }
-                fwrite(&substring_len, 1, sizeof(int), stdout);
-                fprintf(stdout, "%c", text[left]);
-                left = right;
-            }
-        }
-        if (debug == 1)
-        {
-            printf("\n");
-        }
+        fprintf(stdout, "%c", left);
+        left = right;
     }
 
-    free(text);
+    return 0;
 }
 
 int main(int argc, char *argv[])
@@ -51,42 +40,59 @@ int main(int argc, char *argv[])
     FILE *fp = NULL;
     FILE *ofp = NULL;
     char c;
-
+    
     if (argc < 2)
     {
         printf("wzip: file1 [file2 ...]\n");
         return 1;
     }
 
-    fp = fopen(argv[1], "r+");
-
-    if (fp == NULL)
+    if (argc == 2)
     {
-        printf("wzip: cannot open file\n");
-        return 1;
-    }   
+        fp = fopen(argv[1], "r");
 
-    for (i = 2; i < argc; i++)
-    {
-        ofp = fopen(argv[i], "r");
-
-        if (ofp == NULL)
+        if (fp == NULL)
         {
             printf("wzip: cannot open file\n");
-            fclose(fp);
             return 1;
-        }
+        }   
+    }
+    else
+    {
+        fp = fopen("tests-out/combined", "w+");
 
-        while ((c = fgetc(ofp)) != EOF)
+        if (fp == NULL)
         {
-            fputc(c, fp);
-        }
+            printf("failed to create master file\n");
+            return 1;
+        }   
 
-        fclose(ofp);
+        for (i = 1; i < argc; i++)
+        {
+            ofp = fopen(argv[i], "r");
+
+            if (ofp == NULL)
+            {
+                printf("wzip: cannot open file\n");
+                fclose(fp);
+                return 1;
+            }
+
+            while ((c = fgetc(ofp)) != EOF)
+            {
+                fputc(c, fp);
+            }
+
+            fclose(ofp);
+        }
+        rewind(fp);
     }
 
-    rewind(fp);
-    compress(fp);
+    if (compress(fp) == 1)
+    {
+        return 1;
+    }
+
     fclose(fp);
 
     return 0;
